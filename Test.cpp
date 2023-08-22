bac8c5cdc0f3378389bb65df66a8f93e97863fda
Mo-AShraaf <74676105+Mohamed-Ashraf-2210@users.noreply.github.com>
2023-08-23 02:16:57 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -43,76 +43,9 @@ void AShraaf(){
     //freopen("name.in", "r", stdin);
 }
 
-template <typename T> T myMax(T x, T y)
-{
-    return (x > y) ? x : y;
-    //cout<<myMax<int>(2,6);
-}
-
-int Mod(int sum , int k){
-    return (sum%k + k) % k;
-}
-
-/*
-// if you want num bigger move l
-// else move r
-// best with right condition
-int binary_search(vector<int>& nums , int target){
-    int l = 0, r = nums.size() - 1, index_ans = -1;
-    while(l <= r){
-        int mid = l + (r - l) / 2;
-        (nums[mid] < target) ? l = mid + 1 , index_ans = mid : r = mid - 1;
-    }
-    return index_ans;
-}
-
-typedef bitset<12> MASK;
-int memo[1 << 12];
-
-int best (MASK mask){
-    int &res = memo[mask.to_ullong()];
-    if(~res) return res;
-    res = 0;
-    for(int i = 0; i < 12; i++){
-        MASK NewMask = mask;
-        NewMask[i] = 1;
-        res = best(NewMask);
-    }
-    return res;
-}
-*/
-
-ll n , x , p;
-
-bool binary_search(ll need){
-    ll l = 1, r = p;
-    while(l <= r){
-        ll mid = l + (r - l) / 2;
-
-        ll k = mid * (mid+1) / 2;
-        if( k == need or (k+x) % n == 0) return 1;
-        if(k > need)
-            r = mid - 1;
-        else
-            l = mid + 1;
-    }
-    return 0;
-}
 
 void solve(){
-
-    cin >> n >> x >> p;
-    ll need = n - x;
-    rep(i,0,500){
-        if(binary_search(need)) {
-            cout<<"YES";dl
-            return;
-        }
-        need += n;
-    }
-
-    cout<<"NO";dl
-
+    cout << "Yes" << " ";
 }
 
 int main() {
